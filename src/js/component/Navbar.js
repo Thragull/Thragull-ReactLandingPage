798cd501eb4364de801db01b7e209d29abba44b3
Thragull <146970246+Thragull@users.noreply.github.com>
2024-02-05 18:10:09 +0000

    Navbar funcional, pendiente de mejoras

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import thragullLogo from '../../img/favicon.jpg'
 
 const Navbar = () => {
   return (
-    <button className="btn btn-danger">Navbar</button>
+    <nav className='navbar navbar-expand-lg bg-secondary'>
+      <div className='container-fluid'>
+        <a className='navbar-brand text-white ms-lg-3'><img className='rounded-5 me-2' src={thragullLogo}/><span className='fs-4 align-middle fw-bold'>THRAGULL</span></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSelector" aria-controls="navbarSelector" aria-expanded="false" aria-label="Toggle Navigator"><span className="navbar-toggler-icon"></span></button>
+        <div className="collapse navbar-collapse" id="navbarSelector">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className='nav-item'>
+              <a className="nav-link text-white active text-start ms-5 ms-lg-0" aria-current="page" href="#">Home</a>
+            </li>
+            <li className='nav-item'>
+              <a className="nav-link text-white active text-start ms-5 ms-lg-0" aria-current="page" href="#">About</a>
+            </li>
+            <li className='nav-item'>
+              <a className="nav-link text-white active text-start ms-5 ms-lg-0" aria-current="page" href="#">Services</a>
+            </li>
+            <li className='nav-item'>
+              <a className="nav-link text-white active text-start ms-5 ms-lg-0 me-lg-3" aria-current="page" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
